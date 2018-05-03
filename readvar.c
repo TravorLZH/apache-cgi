65d6d9567378552a2797b150e15c17e4f627e8eb
Travor Liu <travor_lzh@outlook.com>
2018-05-03 09:18:56 -0400

    Decode URL

--- readvar.c
+++ readvar.c
@@ -2,14 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <get.h>
+#include <url.h>
+
+char buf[BUFSIZ];
 
 int main(void)
 {
 	char *query=getenv("QUERY_STRING");
 	int len=strlen(query);
+	char *val=get_variable("hello");
 	int i;
 	printf("Content-Type: text/plain\n\n");
 	printf("Query String: %s\n",query);
-	printf("Value of hello: %s\n",get_variable("hello"));
+	printf("Value of hello: %s\n",val);
+	url_decode(val,strlen(val),buf,BUFSIZ);
+	printf("After decode: %s\n",buf);
 	return 0;
 }
