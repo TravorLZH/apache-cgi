9a51ffdd614f3926f6416fe8c770318babed19e0
Travor Liu <travor_lzh@outlook.com>
2018-05-03 02:10:13 -0400

    Common CGI headers

---
+++ include/get.h
@@ -0,0 +1,5 @@
+#ifndef	__GET_H_
+#define	__GET_H_
+extern char **get_variables(void);
+extern char *get_variable(char *name);
+#endif
---
+++ include/url.h
@@ -0,0 +1,4 @@
+#ifndef	__URL_H_
+#define	__URL_H_
+extern int url_decode(const char *url,int url_size,char *result,int result_size);
+#endif
