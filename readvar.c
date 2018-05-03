c00491431ff29c2db6cc1d43c571a04f888e4338
Travor Liu <travor_lzh@outlook.com>
2018-05-03 07:51:10 -0400

    Remove tokenization at first

--- readvar.c
+++ readvar.c
@@ -10,16 +10,6 @@ int main(void)
 	int i;
 	printf("Content-Type: text/plain\n\n");
 	printf("Query String: %s\n",query);
-	strtok(query,"&");
-	printf("Tokenized input\n");
-	for(i=0;i<len;i++){
-		if(query[i]=='\0'){
-			putchar('\n');
-		}else{
-			putchar(query[i]);
-		}
-	}
-	putchar('\n');
 	printf("Value of hello: %s\n",get_variable("hello"));
 	return 0;
 }
