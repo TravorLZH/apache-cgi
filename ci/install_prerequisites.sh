cef09a7815ff70a59f2ce8891affb340d2d8dc61
Travor Liu <travor_lzh@outlook.com>
2018-05-04 13:51:40 -0400

    use pushd/popd instead of cd

--- ci/install_prerequisites.sh
+++ ci/install_prerequisites.sh
@@ -6,28 +6,32 @@ APACHE_PREFIX=/usr/local/apache2
 # APR
 wget http://mirror.olnevhost.net/pub/apache//apr/apr-1.6.3.tar.gz
 tar zxf apr-1.6.3.tar.gz
-cd apr-1.6.3.tar.gz
+pushd apr-1.6.3.tar.gz
 ./configure --prefix=$APR_PREFIX
 make -j20
 sudo make install
+popd
 # APR-UTIL
 wget http://mirror.olnevhost.net/pub/apache//apr/apr-util-1.6.1.tar.gz
 tar zxf apr-util-1.6.1.tar.gz
-cd apr-util-1.6.1
+pushd apr-util-1.6.1
 ./configure --prefix=$APRUTIL_PREFIX --with-apr=$APR_PREFIX
 make -j20
 sudo make install
+popd
 # PCRE
 wget https://ftp.pcre.org/pub/pcre/pcre-8.42.tar.gz
 tar zxf pcre-8.42.tar.gz
-cd pcre-8.42
+pushd pcre-8.42
 ./configure --prefix=$PCRE_PREFIX
 make -j20
 sudo make install
+popd
 # Finally, httpd
 wget http://apache.mirrors.pair.com//httpd/httpd-2.4.33.tar.gz
 tar zxf httpd-2.4.33.tar.gz
-cd httpd-2.4.33
+pushd httpd-2.4.33
 ./configure --prefix=$APACHE_PREFIX --with-apr=$APR_PREFIX --with-apr-util=$APRUTIL_PREFIX --with-pcre=$PCRE_PREFIX
 make -j20
 sudo make install
+popd
